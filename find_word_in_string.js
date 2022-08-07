11035172a4e35e19c64ff2552d2108b71902efec
Sojib Mondol <57976126+sojib-sudo@users.noreply.github.com>
2022-08-07 18:55:07 +0600

    Finding word from a String in JS

---
+++ find_word_in_string.js
@@ -0,0 +1,8 @@
+const mystr = 'sada kala lala mala fala kabila rokeya';
+const find = 'mala';
+
+if(mystr.indexOf(find) !== -1){
+    console.log('Exists inside the string');
+} else{
+    console.log('Can not find it');
+}
